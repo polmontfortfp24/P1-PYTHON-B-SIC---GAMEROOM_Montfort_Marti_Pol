4d68d9284d00408cca4c1a589aed8454b9685619
Pol Montfort <polmontfortfp24@ibf.cat>
2024-09-25 20:56:23 +0200

    menu inicio creado

---
+++ gameroom.py
@@ -0,0 +1,23 @@
+seguirJugando = True
+while seguirJugando:
+    print("Bienvenido a game room!")
+    print("Qué quieres a hacer?")
+    print("1. Jugar a adivinar el numero")
+    print("2. Jugar al piedra, papel o tijera")
+    print("3. Jugar al ahorcado")
+    print("4. Salir")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("Empezamos el juego de adivinar el numero")
+elif choice == "2":
+    print("Empezamos el juego de jugar al ahorcado")
+elif choice == "3":
+    print("Empezamos el juego de jugar al tijera")
+elif choice == "4":
+    print("Hasta luego")
+    seguirJugando = False
+else:
+    print("Ops, no has elegido una opción valida")
+
